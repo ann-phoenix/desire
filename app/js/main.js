d536564d811ab39289ea28d10d34116583e3d555
Ann <ann.phoenix.it@gmail.com>
2021-05-18 17:31:08 +0300

    add rightside menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function () {
 
+	// close and open header__btn
+	$('.header__btn').on('click', function() {
+		$('.rightside-menu').removeClass('rightside-menu--close');
+	});
+	$('.rightside-menu__close').on('click', function() {
+		$('.rightside-menu').addClass('rightside-menu--close');
+	});
+
 })
